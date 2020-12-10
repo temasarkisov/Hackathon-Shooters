4d9cf06cb32d2707a18c5e7c9b0439b85dd0a96c
Artem Sarkisov <sarkisov@artlebedev.ru>
2020-12-10 15:49:46 +0300

    Add main.py in backend

---
+++ src/backend/main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+
+
+def build_model_graph(input_shape=(40,)):
+    model = Sequential()
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_labels))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+    return model
+
+#model = keras.models.load_model("../training/model/shooters_model.h5")
+model = build_model_graph()
+model.load_weights("../training/model/shooters_model.h5")
+
+
