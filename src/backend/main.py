729ffbef632ddc6d58ed030412e269369ae72e69
Artem Sarkisov <sarkisov@artlebedev.ru>
2020-12-10 15:55:14 +0300

    Add main.py in backend

--- src/backend/main.py
+++ src/backend/main.py
@@ -7,6 +7,7 @@ from keras.layers import Dense, Dropout, Activation
 
 
 def build_model_graph(input_shape=(40,)):
+    #num_labels = ...
     model = Sequential()
     model.add(Dense(256))
     model.add(Activation('relu'))
@@ -22,5 +23,7 @@ def build_model_graph(input_shape=(40,)):
 #model = keras.models.load_model("../training/model/shooters_model.h5")
 model = build_model_graph()
 model.load_weights("../training/model/shooters_model.h5")
+
+model.predict('test_input')
 
 
